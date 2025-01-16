203ff982c3da65737c307dc384cc6e21f0fb5dc0
ezekiel-spec <aug24005@byui.edu>
2025-01-16 10:31:02 +0100

    done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const currentYearSpan = document.getElementById("currentyear");
-    const lastModifiedParagraph = document.getElementById("lastModified");
-  
-    const currentYear = new Date().getFullYear();
-    currentYearSpan.textContent = currentYear;
-  
-    const lastModifiedDate = new Date(document.lastModified);
-    lastModifiedParagraph.textContent = `Last modified: ${lastModifiedDate.toLocaleString()}`;
-  });
-  
+  const currentYearSpan = document.getElementById("currentyear");
+  const lastModifiedParagraph = document.getElementById("lastModified");
+
+  const currentYear = new Date().getFullYear();
+  currentYearSpan.textContent = ` ${currentYear}`;
+
+  const lastModifiedDate = new Date(document.lastModified);
+  lastModifiedParagraph.textContent = `Last modified: ${lastModifiedDate.toLocaleString()}`;
+});
